14eed10c4576ca6b41d77c87aae464f306b375e4
Ivan Torres <ivantorresmia27@gmail.com>
2020-10-22 12:58:27 -0700

    working on the orm function and fixing bugs

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES ("Krabby Patty");
+INSERT INTO burgers (burger_name) VALUES ("Whopper Lit");
+INSERT INTO burgers (burger_name) VALUES ("Mo' Cheese Burger");
